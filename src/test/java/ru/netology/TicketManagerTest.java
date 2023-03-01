e526902f0db43b3426811a398002c992a25e6e24
Anastasia <kharitonova.9898@mail.ru>
2023-03-01 23:49:08 +0300

    Fourth change

--- src/test/java/ru/netology/TicketManagerTest.java
+++ src/test/java/ru/netology/TicketManagerTest.java
@@ -80,6 +80,43 @@ class TicketManagerTest {
 
     @Test
 
+    public void noMatchingNames() {   //Не найденно билетов по названию
+
+        TicketRepository repo = new TicketRepository();
+        TicketManager manager = new TicketManager(repo);
+
+        Ticket ticket1 = new Ticket(1, 12_000, "MOW", "AER", 225);    // Москва - Сочи
+        Ticket ticket2 = new Ticket(2, 5_000, "MOW", "LED", 75);      // Москва - Санкт-Петербург
+        Ticket ticket3 = new Ticket(3, 85_000, "DXB", "MEX", 1_260);  // Дубай-Мексика
+        Ticket ticket4 = new Ticket(4, 11_000, "MOW", "AER", 225);    // Москва - Сочи
+        Ticket ticket5 = new Ticket(5, 6_000, "MOW", "LED", 75);      // Москва - Санкт-Петербург
+        Ticket ticket6 = new Ticket(6, 93_000, "DXB", "MEX", 1_260);  // Дубай-Мексика
+        Ticket ticket7 = new Ticket(7, 14_000, "MOW", "AER", 225);    // Москва - Сочи
+        Ticket ticket8 = new Ticket(8, 4_000, "MOW", "LED", 75);      //Москва - Санкт-Петербург
+        Ticket ticket9 = new Ticket(9, 13_000, "KJA ", "DYU", 240);   //Красноярск — Душанбе
+        Ticket ticket10 = new Ticket(10, 12_000, "MOW", "AER", 225);  //Москва - Сочи
+        Ticket ticket11 = new Ticket(11, 12_300, "MOW", "AER", 225);    //Москва - Сочи
+
+        manager.add(ticket1);
+        manager.add(ticket2);
+        manager.add(ticket3);
+        manager.add(ticket4);
+        manager.add(ticket5);
+        manager.add(ticket6);
+        manager.add(ticket7);
+        manager.add(ticket8);
+        manager.add(ticket9);
+        manager.add(ticket10);
+        manager.add(ticket11);
+
+        Ticket[] actual = manager.findAll("KHV" , "CN");
+        Ticket[] expected = {};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+
     public void foundOneTicket() {   //Найден один билет
 
         TicketRepository repo = new TicketRepository();
